55161303c2e181db82de127d8640ff2280cf35b2
Nathan Costa <ncosta6@umassd.edu>
2026-03-01 21:43:39 -0500

    tags backend

--- src/app/api/feed/route.ts
+++ src/app/api/feed/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { pool } from '@/lib/db'
 import { createClient } from '@/lib/supabase/server';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
     try {
         const supabase = await createClient();
         const { data: { user }, error: authError } = await supabase.auth.getUser();
@@ -10,19 +10,74 @@ export async function GET() {
         if (authError || !user) {
             return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         }
-
         const userId = user.id; 
-        const feedQuery = `
-            SELECT DISTINCT R.*
-            FROM users U, releases R, follows F, artist_releases AR
-            WHERE U.id = $1
-                AND F.artist_id = AR.artist_id
-                AND AR.release_id = R.id
-            ORDER BY release_date DESC
-            LIMIT 50;
-        `;
-        
-        const { rows } = await pool.query(feedQuery, [userId])
+
+        const tagParam = request.nextUrl.searchParams.get('tag');
+        const scopeParam = request.nextUrl.searchParams.get('scope');
+
+        if (scopeParam && scopeParam !== 'artist' && scopeParam !== 'release') {
+            return NextResponse.json(
+                { error: "Invalid scope parameter. Must be 'artist' or 'release'." }, 
+                { status: 400 }
+            );
+        }
+
+        let rows;
+        if (tagParam && scopeParam === "artist") {
+            const feedQuery = `
+                SELECT DISTINCT R.*
+                FROM users U, releases R, follows F, artist_releases AR, artist_tags AT, tags T
+                WHERE U.id = $1                      -- Filter for current user
+                    AND F.user_id = U.id             -- Join follows & users
+                    AND F.artist_id = AR.artist_id   -- Join artist_releases & follows (artist_id from followed artist)
+                    AND AR.release_id = R.id         -- Join releases & artist_releases
+                    AND F.artist_id = AT.artist_id   -- Join artist_tags to the followed artist
+                    AND AT.tag_id = T.id             -- Join artist_tags & tags
+                    AND T.name ILIKE $2              -- Filter tags by name
+                    AND T.scope = 'artist'           -- Filter tags by scope
+                    AND T.user_id = U.id             -- Filter tags by user
+                ORDER BY R.release_date DESC
+                LIMIT 200;
+            `;
+            ({ rows } = await pool.query(feedQuery, [
+                userId, 
+                tagParam
+            ]));
+
+        } else if (tagParam && scopeParam === "release"){
+            const feedQuery = `
+                SELECT DISTINCT R.*
+                FROM users U, releases R, follows F, artist_releases AR, release_tags RT, tags T
+                WHERE U.id = $1                      -- Filter for current user
+                    AND F.user_id = U.id             -- Join follows & users
+                    AND F.artist_id = AR.artist_id   -- Join artist_releases & follows
+                    AND AR.release_id = R.id         -- Join releases & artist_releases
+                    AND R.id = RT.release_id         -- Join release_tags to the specific release
+                    AND RT.tag_id = T.id             -- Join release_tags & tags
+                    AND T.name ILIKE $2              -- Filter tags by name
+                    AND T.scope = 'release'          -- Filter tags by scope
+                    AND T.user_id = U.id             -- Filter tags by user
+                ORDER BY R.release_date DESC
+                LIMIT 200;
+            `;
+            ({ rows } = await pool.query(feedQuery, [
+                userId, 
+                tagParam
+            ]));
+
+        } else {
+            const feedQuery = `
+                SELECT DISTINCT R.*
+                FROM users U, releases R, follows F, artist_releases AR
+                WHERE U.id = $1
+                    AND F.user_id = U.id
+                    AND F.artist_id = AR.artist_id
+                    AND AR.release_id = R.id
+                ORDER BY R.release_date DESC
+                LIMIT 200;
+            `;          
+            ({ rows } = await pool.query(feedQuery, [userId]));
+        }
 
         return NextResponse.json({ success: true, feed: rows});
 
--- src/app/api/tags/route.ts
+++ src/app/api/tags/route.ts
@@ -76,13 +76,8 @@ export async function POST(request: NextRequest) {
     }
 }
 
-
-export async function DELETE(request: NextRequest) {
-    const client = await pool.connect();
-
+export async function GET(request: NextRequest) { 
     try {
-        await client.query('BEGIN');
-        
         const supabase = await createClient();
         const { data: { user }, error: authError } = await supabase.auth.getUser();
 
@@ -92,61 +87,112 @@ export async function DELETE(request: NextRequest) {
 
         const userId = user.id; 
         
-        const body = await request.json();
-        const tagName = body.tagName.toString();
-        const itunesId = body.iTunesId.toString(); 
-        const scope = body.scope.toString();
+        const scopeParam = request.nextUrl.searchParams.get('scope');
 
-        const insertTagQuery = `
-            INSERT INTO tags (user_id, scope, name) 
-            VALUES ($1, $2, $3)
-            ON CONFLICT (user_id, scope, name)
-            DO UPDATE SET name = EXCLUDED.name
-            RETURNING id;
+        let tagsQuery = `
+            SELECT id, name, scope
+            FROM tags
+            WHERE user_id = $1
         `;
+        const queryParams = [userId];
 
-        const insertTagQueryResult = await client.query(insertTagQuery, [
-            userId, 
-            scope,
-            tagName
-        ]);
+        if (scopeParam === 'artist' || scopeParam === 'release') { 
+            tagsQuery += ` AND scope = $2`;
+            queryParams.push(scopeParam);
+        } else if (scopeParam) {
+            return NextResponse.json(
+                { error: "Invalid scope parameter. Must be 'artist' or 'release'." }, 
+                { status: 400 }
+            );
+        }
+        tagsQuery += ` ORDER BY name ASC`;
+        const { rows } = await pool.query(tagsQuery, queryParams);
 
-        const internalTagId = insertTagQueryResult.rows[0].id;
+        return NextResponse.json({ success: true, tags: rows});
 
+    } catch (error: unknown) {
+        console.error("Error retrieving tags:", error); 
+
+        const message = error instanceof Error ? error.message : "An unknown error occurred.";
+
+        return NextResponse.json(
+            { error: "Failed to retrieve tags", details: message }, 
+            { status: 500 }   
+        );
+
+    }
+}
+
+export async function DELETE(request: NextRequest) {
+    const supabase = await createClient();
+    const { data: { user }, error: authError } = await supabase.auth.getUser();
+
+    if (authError || !user) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const userId = user.id; 
+    const tagName = request.nextUrl.searchParams.get('tagName');
+    const itunesId = request.nextUrl.searchParams.get('itunesId');
+    const scope = request.nextUrl.searchParams.get('scope');
+
+    if (!tagName || !itunesId || !scope) {
+        return NextResponse.json({ error: "Missing required fields: tagName, iTunesId, or scope." }, { status: 400 });
+    }
+    if (scope !== 'artist' && scope !== 'release') {
+        return NextResponse.json({ error: "Invalid scope. Must be 'artist' or 'release'." }, { status: 400 });
+    }
+
+    try {        
         if (scope === 'artist') {
-            const insertArtistTagQuery = `
-                INSERT INTO artist_tags (tag_id, artist_id) 
-                SELECT $1, id FROM artists WHERE itunes_id = $2
-                ON CONFLICT (tag_id, artist_id) DO NOTHING
+            const unTagQuery = `
+                DELETE from artist_tags 
+                WHERE artist_id = (SELECT id FROM artists WHERE itunes_id = $1)
+                    AND tag_id = (
+                      SELECT id FROM tags 
+                      WHERE user_id = $2 
+                        AND scope = $3 
+                        AND name = $4
+                    )
             `;
 
-            await client.query(insertArtistTagQuery, [internalTagId, itunesId]);
+            await pool.query(unTagQuery, [
+                itunesId,
+                userId,
+                scope,
+                tagName
+            ]);
 
         } else {
-            const insertReleaseTagQuery = `
-                INSERT INTO release_tags (tag_id, release_id) 
-                SELECT $1, id FROM releases WHERE itunes_id = $2
-                ON CONFLICT (tag_id, release_id) DO NOTHING
+            const unTagQuery = `
+                DELETE from release_tags 
+                WHERE release_id = (SELECT id FROM releases WHERE itunes_id = $1)
+                    AND tag_id = (
+                      SELECT id FROM tags 
+                      WHERE user_id = $2 
+                        AND scope = $3 
+                        AND name = $4
+                    )
             `;
 
-            await client.query(insertReleaseTagQuery,[internalTagId, itunesId]);
+            await pool.query(unTagQuery, [
+                itunesId,
+                userId,
+                scope,
+                tagName
+            ]);
+
         }
         
-        await client.query('COMMIT');
         return NextResponse.json({ success: true });
 
     } catch (error: unknown) {
-        await client.query('ROLLBACK');
-        
         console.error("Error:", error);
         const message = error instanceof Error ? error.message : "An unknown error occured.";
 
         return NextResponse.json(
-            { error: "Failed to add tag", details: message }, 
+            { error: "Failed to untag", details: message }, 
             { status: 500 }   
         );
-
-    } finally {
-        client.release();
     }
 }
